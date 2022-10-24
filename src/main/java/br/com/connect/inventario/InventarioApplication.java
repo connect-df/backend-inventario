5ad1541ddda312b7abd5b29e7c64603c483b553a
brunostaine <bruno.oliveira@connectdf.com.br>
2022-10-24 11:59:09 -0300

    Spring projeto iniciado

---
+++ src/main/java/br/com/connect/inventario/InventarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.connect.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioApplication.class, args);
+	}
+
+}
